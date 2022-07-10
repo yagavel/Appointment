58bc829c7ca9e723c4e68cbe78488cafc8e04e53
Yagavel P <yagavel.p@roamsoft.in>
2022-07-10 21:33:32 +0530

    appjs modifiedhj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Payment from './Payment'
+import Payment from './Payment';
+import Car from './Car'
 
 function App() {
   return (
      <Payment />
+     <Car />
   );
 }
 export default App;
---
+++ src/Car.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Car() {
+  return <h2>Hi, I am a Car!</h2>;
+}
+
+export default Car;
